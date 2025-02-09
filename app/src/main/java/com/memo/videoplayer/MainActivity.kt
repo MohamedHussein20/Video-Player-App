bf5980992b42614cf4b94aa9411819e401701350
Mohamed Hussein <mohussein1279@gmail.com>
2025-02-09 22:22:25 +0200

    Exo VideoPlayer Added

--- app/src/main/java/com/memo/videoplayer/MainActivity.kt
+++ app/src/main/java/com/memo/videoplayer/MainActivity.kt
@@ -1,32 +1,24 @@
 package com.memo.videoplayer
 
-import android.net.Uri
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val videoUrl =
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
             Column {
-                YoutubePlayer(
-                    youtubeVideoId = "VZGMXcVafoE",
-                    lifeCycleOwner = this@MainActivity
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                VideoPlayer(
-                    videoUri = Uri.parse(
-                        "android.resource://com.memo.videoplayer/raw/video_mp4"
-                    )
-                )
+            VideoPlayerExo(
+                videoUri = videoUrl
+            )
             }
         }
     }
